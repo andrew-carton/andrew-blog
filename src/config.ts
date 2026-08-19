ae76339ede84f8d35485ee904920fa36d8bbce56
Andrew Carton <37844256+andrew-carton@users.noreply.github.com>
2026-08-19 13:13:57 +0100

    h
    h

--- src/config.ts
+++ src/config.ts
@@ -25,6 +25,7 @@ const config: ResolvedAstroPaperConfig = {
     perIndex: userConfig.posts?.perIndex ?? 4,
     scheduledPostMargin:
       userConfig.posts?.scheduledPostMargin ?? 15 * 60 * 1000,
+	entries: "./",
   },
   features: {
     lightAndDarkMode: userConfig.features?.lightAndDarkMode ?? true,
