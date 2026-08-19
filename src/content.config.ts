276c962a7886d4e3bf74bf254d4abbbaadd1720d
Andrew Carton <37844256+andrew-carton@users.noreply.github.com>
2026-08-19 08:14:02 +0100

    g
    g

--- src/content.config.ts
+++ src/content.config.ts
@@ -7,21 +7,22 @@ export const BLOG_PATH = "src/content/posts";
 
 const posts = defineCollection({
   loader: glob({ pattern: "**/[^_]*.{md,mdx}", base: `./${BLOG_PATH}` }),
-  schema: ({ image }) =>
-    z.object({
-      author: z.string().default(config.site.author),
-      pubDatetime: z.date().optional(),
-      modDatetime: z.date().optional().nullable(),
-      title: z.string(),
-      featured: z.boolean().optional(),
-      draft: z.boolean().optional(),
-      tags: z.array(z.string()).default(["others"]),
-      ogImage: image().or(z.string()).optional(),
-      description: z.string(),
-      canonicalURL: z.string().optional(),
-      hideEditPost: z.boolean().optional(),
-      timezone: z.string().optional(),
-    }),
+	schema: ({ image }) =>
+	  z.object({
+		author: z.string().default(config.site.author),
+		pubDatetime: z.date().optional(),
+		modDatetime: z.date().optional().nullable(),
+		title: z.string().optional(),
+		featured: z.boolean().optional(),
+		draft: z.boolean().optional(),
+		tags: z.array(z.string()).default(["others"]),
+		ogImage: image().or(z.string()).optional(),
+		description: z.string().optional(),
+		canonicalURL: z.string().optional(),
+		hideEditPost: z.boolean().optional(),
+		timezone: z.string().optional(),
+	  }),
+
 });
 
 const pages = defineCollection({
