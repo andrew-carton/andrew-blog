62c728c9cba4a19c59e1d06c82808d3736875f2b
Andrew Carton <37844256+andrew-carton@users.noreply.github.com>
2026-08-19 12:45:55 +0100

    g
    g

--- astro-paper.config.ts
+++ astro-paper.config.ts
@@ -24,7 +24,7 @@ export default defineAstroPaperConfig({
     showBackButton: true,
     editPost: {
       enabled: false,
-      url: "https://github.com/andrew-carton/andrew-blog/edit/main/",
+      url: "https://github.com/andrew-carton/andrew-blog/",
     },
     search: "pagefind",
   },
